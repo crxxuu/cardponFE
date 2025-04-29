870ae7e5578f5702d114f618e6c288f28dfd884f
crxxuu <101150127+crxxuu@users.noreply.github.com>
2025-04-29 13:22:28 -0700

    Update registrationpage.js

--- src/registrationpage.js
+++ src/registrationpage.js
@@ -20,7 +20,7 @@ const navigate=useNavigate()
 
   const discordLoginParams = {
     clientId: '1353009606363709480',
-    redirectUri: 'http://localhost:3000/Signup',
+    redirectUri: 'https://www.cardpon.com/Signup',
     responseType: 'token', 
     scopes: ['identify', 'email'],
     onSuccess: response => {
